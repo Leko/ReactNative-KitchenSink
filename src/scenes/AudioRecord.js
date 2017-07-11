40bbcebbb04345d50ce9c5c34f8031aa46f8dd88
Leko <leko.noor@gmail.com>
2017-07-12 00:17:25 +0900

    feat: Implement recording start/stop

--- src/scenes/AudioRecord.js
+++ src/scenes/AudioRecord.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react'
 import {
   Platform,
+  StyleSheet,
   View,
   Text,
   TouchableOpacity,
   PermissionsAndroid
 } from 'react-native'
 import { AudioRecorder, AudioUtils } from 'react-native-audio'
-import { some } from '../functions'
+import flow from 'lodash/flow'
+import { is, some, digger } from '../functions'
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  button: {
+    padding: 10,
+    paddingHorizontal: 20,
+  },
+})
 
 export default class AudioRecord extends Component {
-  constructor (props) {
-    super(props)
-    this.handleRecord = this.handleRecord.bind(this)
+  static defaultProps = {
+    audioPath: AudioUtils.DocumentDirectoryPath + 'voice.aac'
   }
 
-  async prepare () {
+  constructor (props) {
+    super(props)
+    this.handleStart = this.handleStart.bind(this)
+    this.handleStop = this.handleStop.bind(this)
+    this.state = {
+      recording: false,
+      recorded: false,
+    }
   }
 
   async requestPermission () {
@@ -31,24 +49,78 @@ export default class AudioRecord extends Component {
     })
   }
 
-  async handleRecord () {
+  async setUp () {
+    const options = {
+      SampleRate: 22050,    // CDと同じ
+      Channels: 1,          // モノラル
+      AudioQuality: 'Low',  // 
+      AudioEncoding: 'aac', // iOS/Android両対応のフォーマット
+    }
+
+    return AudioRecorder.prepareRecordingAtPath(this.props.audioPath, options)
+  }
+
+  async record () {
+    return await AudioRecorder.startRecording()
+  }
+
+  async tearDown (success) {
+    this.setState({ recording: false, recorded: success })
+  }
+
+  async handleStart () {
     try {
       if (!await this.requestPermission()) {
         alert('TODO: Handle permission denied')
       }
 
-      alert('Ready')
+      await this.setUp()
+      await this.record()
+      this.setState({ recording: true, recorded: false })
     } catch (e) {
       console.error(e)
     }
   }
 
+  async handleStop () {
+    if (!this.state.recording) {
+      return
+    }
+
+    const filePath = await AudioRecorder.stopRecording()
+    if (Platform.OS === 'android') {
+      await this.tearDown(true, filePath)
+    }
+  }
+
+  componentDidMount () {
+    const isOK = flow(digger('status'), is('OK'))
+    AudioRecorder.onFinished = (data) => {
+      if (Platform.OS !== 'ios') {
+        return
+      }
+      this.tearDown(isOK(data))
+    }
+  }
+
   render () {
     return (
-      <View>
-        <TouchableOpacity onPress={this.handleRecord}>
-          <Text>{'録音する'}</Text>
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.handleStart}
+        >
+          <Text>{'録音を開始する'}</Text>
         </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.handleStop}
+        >
+          <Text>{'録音を停止する'}</Text>
+        </TouchableOpacity>
+
+        <Text>State:</Text>
+        <Text>{JSON.stringify(this.state, null, 2)}</Text>
       </View>
     )
   }
